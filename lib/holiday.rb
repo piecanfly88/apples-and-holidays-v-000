54dc949de80c2c2fe693ea3edccde173bc64c8e8
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-04-12 06:05:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, season_holidays_hash|
+    puts "#{season}:"
+    season_holidays_hash.each do |holiday, attributes|
+      puts "  #{holiday.to_s.split("_").each {|word| word.capitalize!}.join(" ")}: #{attributes.join(", ")}"
 
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
